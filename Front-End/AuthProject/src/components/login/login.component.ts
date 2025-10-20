7cdc5a2d70f6c8c01b079e426c2ef6b3ffbd82bf
Sobhan <ziaey@gmail.com>
2025-10-20 07:47:15 +0330

    feat: implement login component with form validation and styling

--- Front-End/AuthProject/src/components/login/login.component.ts
+++ Front-End/AuthProject/src/components/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login data:', this.loginForm.value);
+      // Handle login logic here
+    }
+  }
 }
