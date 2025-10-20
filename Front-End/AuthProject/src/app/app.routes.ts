57563084c352eb739441c07e1d399b7855a78e92
Sobhan <ziaey@gmail.com>
2025-10-20 07:42:04 +0330

    feat: implement routing and create initial components for login, register, home, and profile

--- Front-End/AuthProject/src/app/app.routes.ts
+++ Front-End/AuthProject/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('../components/login/login.component').then(m => m.LoginComponent),
+        pathMatch: 'full'
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('../components/register/register.component').then(m => m.RegisterComponent),
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('../components/home/home.component').then(m => m.HomeComponent),
+        pathMatch: 'full'
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('../components/profile/profile.component').then(m => m.ProfileComponent),
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }
+];
