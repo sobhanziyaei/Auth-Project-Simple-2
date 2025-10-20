0b29fb0f62cc66ada38acbf3592e0c7e1b783ce9
Sobhan <ziaey@gmail.com>
2025-10-20 07:49:46 +0330

    feat: implement registration component with form validation and styling

--- Front-End/AuthProject/src/components/register/register.component.ts
+++ Front-End/AuthProject/src/components/register/register.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Register data:', this.registerForm.value);
+      // Handle register logic here
+    }
+  }
 }
