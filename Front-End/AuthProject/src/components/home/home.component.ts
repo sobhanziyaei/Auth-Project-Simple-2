9869204d5c01c23ae75417331c172fa190abe404
Sobhan <ziaey@gmail.com>
2025-10-20 07:53:31 +0330

    feat: implement home component with user greeting and action buttons

--- Front-End/AuthProject/src/components/home/home.component.ts
+++ Front-End/AuthProject/src/components/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  userName = 'User';
 
+  goToProfile() {
+    console.log('Go to profile');
+    // TODO: navigate to profile route
+  }
+
+  logout() {
+    console.log('Logout');
+    // TODO: add logout logic
+  }
 }
