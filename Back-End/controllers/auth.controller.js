e30e50c78c262bc4c4a54be525bae31fa37fcae8
Sobhan <ziaey@gmail.com>
2025-10-21 07:54:17 +0330

    feat: implement login functionality with token generation and error handling

--- Back-End/controllers/auth.controller.js
+++ Back-End/controllers/auth.controller.js
@@ -1,10 +1,34 @@
 const { userModel } = require("../models/user.model");
-const { hashPassword } = require("../utils/auth.util");
+const { hashPassword, signToken, comparePassword } = require("../utils/auth.util");
+
+
+login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({
+            email,
+        });
+
+        if (!user) {
+            throw { status: 400, message: "کاربری با این ایمیل یافت نشد" };
+        }
+
+        if (comparePassword(password, user.password)) {
+            const token = signToken({ id: user._id, email: user.email });
+            res.send({ token, message: "login successfully" });
+        } else {
+            throw { status: 400, message: "email or password is incorrect" };
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
 
 register = async (req, res, next) => {
     try {
         const { fullName, username, password } = req.body;
-        
+
         // Check if user with this username already exists
         const existingUser = await userModel.findOne({ username });
         if (existingUser) {
@@ -12,7 +36,7 @@ register = async (req, res, next) => {
             error.statusCode = 400;
             return next(error);
         }
-        
+
         const user = await userModel.create({
             fullName,
             username,
@@ -25,5 +49,6 @@ register = async (req, res, next) => {
 }
 
 module.exports = {
+    login,
     register
 }
