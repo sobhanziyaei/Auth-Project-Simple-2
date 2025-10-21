2cfafb02b8a26d84e3773f25a11442925752f587
Sobhan <ziaey@gmail.com>
2025-10-21 08:47:35 +0330

    refactor: restructure authentication components and services
    - Removed old home, login, profile, and register components and their associated styles and templates.
    - Added new home, login, profile, and register components with updated templates and styles.
    - Implemented reactive forms for login and registration processes.
    - Created an AuthService for handling authentication logic.

--- Front-End/AuthProject/src/app/app.routes.ts
+++ Front-End/AuthProject/src/app/app.routes.ts
@@ -3,22 +3,22 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'login',
-        loadComponent: () => import('../components/login/login.component').then(m => m.LoginComponent),
+        loadComponent: () => import('../core/components/login/login.component').then(m => m.LoginComponent),
         pathMatch: 'full'
     },
     {
         path: 'register',
-        loadComponent: () => import('../components/register/register.component').then(m => m.RegisterComponent),
+        loadComponent: () => import('../core/components/register/register.component').then(m => m.RegisterComponent),
         pathMatch: 'full'
     },
     {
         path: 'home',
-        loadComponent: () => import('../components/home/home.component').then(m => m.HomeComponent),
+        loadComponent: () => import('../core/components/home/home.component').then(m => m.HomeComponent),
         pathMatch: 'full'
     },
     {
         path: 'profile',
-        loadComponent: () => import('../components/profile/profile.component').then(m => m.ProfileComponent),
+        loadComponent: () => import('../core/components/profile/profile.component').then(m => m.ProfileComponent),
         pathMatch: 'full'
     },
     {
