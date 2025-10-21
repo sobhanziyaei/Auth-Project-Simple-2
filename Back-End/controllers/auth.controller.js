ca6ba6ae314fe0b107358e6605476fba5446b7ee
Sobhan <ziaey@gmail.com>
2025-10-21 07:26:25 +0330

    feat: add username existence check during user registration

--- Back-End/controllers/auth.controller.js
+++ Back-End/controllers/auth.controller.js
@@ -4,6 +4,15 @@ const { hashPassword } = require("../utils/auth.util");
 register = async (req, res, next) => {
     try {
         const { fullName, username, password } = req.body;
+        
+        // Check if user with this username already exists
+        const existingUser = await userModel.findOne({ username });
+        if (existingUser) {
+            const error = new Error('User with this username already exists');
+            error.statusCode = 400;
+            return next(error);
+        }
+        
         const user = await userModel.create({
             fullName,
             username,
